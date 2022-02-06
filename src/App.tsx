6312c7c0d1c78709e184cb0121ae2b9ba5d7af72
Jackson Queiroz <jack.shinoda5@gmail.com>
2022-02-06 01:47:04 -0300

    app finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Typography, Container, Grid } from '@mui/material';
+import { ToDoColumn } from './components/columns/ToDo';
+import { InProgressColumn } from './components/columns/InProgress';
+import { DoneColumn } from './components/columns/Done';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container>
+      <Typography textAlign='center' variant='h3' mt={3} mb={5}>
+        This is a ToDo APP with Redux
+      </Typography>
+      <Grid container spacing={3} justifyContent='center'>
+        <Grid item md={4}>
+          <ToDoColumn />
+        </Grid>
+        <Grid item md={4}>
+          <InProgressColumn />
+        </Grid>
+        <Grid item md={4}>
+          <DoneColumn />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/columns/InProgress.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import { useSelector } from 'react-redux';
+import { StoreState } from '../../redux/store';
+import { inProgressSlice } from '../../redux/slice/inProgress';
+import ColumnLayout from '../ColumnLayout';
+
+export const InProgressColumn: React.FC = () => {
+  const { inProgress } = useSelector((state: StoreState) => state);
+
+  const {
+    actions: { completeStatus, remove, add, updateTextShowed },
+  } = inProgressSlice;
+
+  return (
+    <>
+      <Typography mb={3}>All inProgress tasks: {inProgress.length}</Typography>
+      <ColumnLayout
+        droppableId='inProgress'
+        labelText="Type 'in progress' item"
+        completedHandler={completeStatus}
+        removeHandler={remove}
+        addHandler={add}
+        selectorState={inProgress}
+        updateTextShowed={updateTextShowed}
+      />
+    </>
+  );
+}
